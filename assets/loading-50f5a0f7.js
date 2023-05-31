a8ca2641009396b4212fb96e501d6f6662dd52d7
JonathanCabuyales <32933782+JonathanCabuyales@users.noreply.github.com>
2023-05-30 21:44:58 -0500

    Updates

---
+++ assets/loading-50f5a0f7.js
@@ -0,0 +1,1 @@
+import{j as s,r as n}from"./index-0e493f63.js";const o=({children:a})=>s.jsx(n.Suspense,{fallback:s.jsx("div",{className:"container",children:s.jsx("h1",{className:"loading",children:"Loading..."})}),children:a});export{o as default};
